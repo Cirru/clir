beeff88edfa6c0f5e06f751daf200efba5e0a215
jiyinyiyong <jiyinyiyong@gmail.com>
2012-03-30 23:43:18 +0800

    测试计算

--- target.c
+++ target.c
@@ -1,18 +1,13 @@
+#include <math.h>
 #include <stdio.h>
+double distance (double  x1, double  y1, double  x2, double  y2){
+  double dx = x2 - x1;
+  double dy = y2 - y1;
+  double dsquared = dx * dx + dy * dy;
+  double result = sqrt (dsquared);
+  return result;
+}
 int main (void ){
-  int x = 3;
-  switch (x){
-    case 1:{
-      printf ("1");
-      break;
-    }
-    case 2:{
-      printf ("2");
-      break;
-    }
-    default:{
-      printf ("else");
-    }
-  }
+  printf ("distance is %f\n", distance(1.0, 2.0, 4.0, 6.0));
   return 0;
 }
